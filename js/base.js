b652e6fcb4e538c64062808d406ac000300a600d
Sean Stopnik <thelifemgmt@gmail.com>
2012-11-07 19:00:20 -0500

    fixes to js and tabs

--- js/base.js
+++ js/base.js
@@ -19,56 +19,14 @@ $('a.scroll').click(function(e){
 
 
 // Tabs
-$(function(){
-	$('#tab1').show();
-	$('#tabButton1').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab1').fadeIn(600);
-		$(this).addClass('active');
-	});
-	$('#tabButton2').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab2').fadeIn(600);
-		$(this).addClass('active');
-	});
-	$('#tabButton3').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab3').fadeIn(600);
-		$(this).addClass('active');
-	});
-	$('#tabButton4').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab4').fadeIn(600);
-		$(this).addClass('active');
-	});
-	$('#tabButton5').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab5').fadeIn(600);
-		$(this).addClass('active');
-	});
-	$('#tabButton6').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab6').fadeIn(600);
-		$(this).addClass('active');
-	});
-	$('#tabButton7').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab7').fadeIn(600);
-		$(this).addClass('active');
-	});
-	$('#tabButton8').click(function(){
-		$('.tab-frame').hide();
-		$('ul.tabs-nav li').removeClass('active');
-		$('#tab8').fadeIn(600);
-		$(this).addClass('active');
-	});
+$('#tab1').show();
+$('ul.tabs-nav li').click(function(){ 
+	$('#tabs ul li').removeClass('active');
+	$(this).addClass('active'); 
+	var currentTab = $(this).find("a[href]").attr('href'); 
+	$('.tab-frame').hide();
+	$(currentTab).fadeIn(600);
+	return false;
 });
 
 
